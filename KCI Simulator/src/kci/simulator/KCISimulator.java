6fc2ceb51f4d04dfd4b9dbfea3728b3809695d78
Abdur Rehman Asad <abdurbinasad@gmail.com>
2025-05-28 14:20:34 -0400

    Update KCISimulator.java

--- KCI Simulator/src/kci/simulator/KCISimulator.java
+++ KCI Simulator/src/kci/simulator/KCISimulator.java
@@ -4,10 +4,5 @@ public class KCISimulator {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-        
-                
-=======
->>>>>>> 76e391e3f9df4db5c630c985ed5e7215222fe824
     } 
 }

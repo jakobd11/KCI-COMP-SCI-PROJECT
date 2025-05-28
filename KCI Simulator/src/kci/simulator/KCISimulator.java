76e391e3f9df4db5c630c985ed5e7215222fe824
jakobd11 <jdombrow2007@gmail.com>
2025-05-28 14:16:39 -0400

    Update KCISimulator.java

--- KCI Simulator/src/kci/simulator/KCISimulator.java
+++ KCI Simulator/src/kci/simulator/KCISimulator.java
@@ -3,12 +3,6 @@ package kci.simulator;
 public class KCISimulator {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-           
-=======
-    
-    
-                
->>>>>>> edits
+
     } 
 }

899a5a455e8da9705f0d62fdb9c41fd2f7335ca2
jakobd11 <jdombrow2007@gmail.com>
2025-05-29 13:59:52 -0400

    Update KCISimulator.java

--- KCI Simulator/src/kci/simulator/KCISimulator.java
+++ KCI Simulator/src/kci/simulator/KCISimulator.java
@@ -8,24 +8,15 @@ import java.util.*;
 public class KCISimulator{
 
     public static JFrame frame;
-    public static JLabel label;
-    public static JTextField txt;
     
     public static void main(String[] args) {        
-        txt = new JTextField();
         frame = new JFrame("Main Menu");
         frame.setSize(600,300);
         frame.setLocation(400,200);
         frame.addKeyListener(new KCIKeyListener());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
-        
-        label = new JLabel("null");
-        label.setFont(new java.awt.Font("Arial", Font.BOLD, 24));
-        label.setBounds(200, 50, 200, 24);
-        label.setForeground(Color.red);
-        frame.add(label);
-        
+                
         frame.setVisible(true);
     } 
 }

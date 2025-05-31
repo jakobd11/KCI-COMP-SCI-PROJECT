7e24bf103e26b9b0b46418307dd7486ae0c65005
jakobd11 <jdombrow2007@gmail.com>
2025-05-31 15:54:54 -0400

    base map
    added placeholder images and small map, need to scale up and easily move between frames

--- KCI Simulator/src/kci/simulator/KCISimulator.java
+++ KCI Simulator/src/kci/simulator/KCISimulator.java
@@ -11,12 +11,33 @@ public class KCISimulator{
     
     public static void main(String[] args) {        
         frame = new JFrame("Main Menu");
-        frame.setSize(350,350);
+        frame.setSize(700,700);
         frame.setLocation(400,200);
+        frame.setLayout(new GridLayout(7,7));
         frame.addKeyListener(new KCIKeyListener());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
-                
+        
+        int[][] mapMatrix = new int[7][7];
+        ImageIcon pic;
+        
+        for (int i = 0; i < mapMatrix.length; i++) {
+            for (int j = 0; j < mapMatrix[i].length; j++) {
+                if (i == 0 || i == 6) {
+                    mapMatrix[i][j] = 0;
+                    pic = new ImageIcon(new ImageIcon("smoke.jpg").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
+                } else if (i == 1 || i == 5) {
+                    mapMatrix[i][j] = 1;
+                    pic = new ImageIcon(new ImageIcon("wall.jpeg").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
+                } else {
+                    mapMatrix[i][j] = 2;
+                    pic = new ImageIcon(new ImageIcon("floor.jpg").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
+                }
+                JLabel label = new JLabel(pic);
+                label.setBounds(0, 0, 100, 100);
+                frame.add(label);
+            }
+        }
+          
         frame.setVisible(true);
     } 
 }

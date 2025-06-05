828212701dd33201319a553757f7d23cf0e9b787
jakobd11 <jdombrow2007@gmail.com>
2025-06-05 13:08:53 -0400

    bfxb

---
+++ KCI Simulator/src/kci/simulator/NPC.java
@@ -0,0 +1,54 @@
+
+
+
+package kci.simulator;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class NPC{
+    protected int npcX, npcY;
+    protected String name;
+    protected JLabel image;
+
+    public NPC(int npcX, int npcY, String name, JLabel image) {
+        this.npcX = npcX;
+        this.npcY = npcY;
+        this.name = name;
+        this.image = image;
+    }
+
+    public int getNpcX() {
+        return npcX;
+    }
+
+    public int getNpcY() {
+        return npcY;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public JLabel getImage() {
+        return image;
+    }
+
+    public void setNpcX(int npcX) {
+        this.npcX = npcX;
+    }
+
+    public void setNpcY(int npcY) {
+        this.npcY = npcY;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setImage(JLabel image) {
+        this.image = image;
+    }
+}
